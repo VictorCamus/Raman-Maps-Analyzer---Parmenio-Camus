4359b38541cb62e8d85a05aa6a435cea5aab634e
P-Boronat <parmenio.boronat@uv.es>
2026-07-13 15:30:40 +0200

    Canvis de la CCD del detector

--- CCD/correction.py
+++ CCD/correction.py
@@ -5,8 +5,6 @@ def ccd_correct(xdata, spectra):
     CCD_Data = read_table('CCD/CCD.csv', delimiter=' ', decimal=',', usecols=[0, 1], header=None)
     QE = interp(xdata, CCD_Data[0], CCD_Data[1], left=nan, right=nan)
     QE[QE < 0.05] = nan
-    print(spectra.shape)
-    print(QE.shape)
     spectra /= QE
 
     return spectra
